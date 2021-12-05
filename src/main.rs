cbe55b001aed971f8480aa3190b700d82517425f
andrewnturner <andrewturner397@gmail.com>
2021-12-05 17:42:19 +0000

    Image manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use image::{RgbImage, Rgb, ImageFormat};
+
 fn main() {
-    println!("Hello, world!");
+    let mut image = RgbImage::new(32, 32);
+
+    image.put_pixel(10, 10, Rgb([255, 0, 0]));
+
+    let path = "out.png";
+    image.save_with_format(path, ImageFormat::Png);
 }
