6d69613f519d74385adda4b701a46a0f612cc096
andrewnturner <andrewturner397@gmail.com>
2021-12-05 18:19:44 +0000

    draw line

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use image::{RgbImage, Rgb, ImageFormat};
+mod drawing;
+
+use image::{ImageFormat, RgbImage, Rgb};
+
+use drawing::line::draw_line;
 
 fn main() {
     let mut image = RgbImage::new(32, 32);
 
-    image.put_pixel(10, 10, Rgb([255, 0, 0]));
+    draw_line(&mut image, Rgb([255, 0, 0]), 10, 10, 20, 25);
 
     let path = "out.png";
     image.save_with_format(path, ImageFormat::Png);
